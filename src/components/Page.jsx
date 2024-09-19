4d87b8ccf880c40f0f64a34ba8444d5c7f873a7f
Leo <leonidasgarcialescano@gmail.com>
2024-09-19 02:27:43 -0500

    cambiando router

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,16 +1,16 @@
 import Login from './Login.jsx'
 import Registrar from './Registrar.jsx'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const Page = () => {
     return (
         <div className="w-screen h-screen bg-slate-700 flex justify-center items-center">
-            <Router>
+            <BrowserRouter>
                 <Routes>
                     <Route path='/' element={<Login />} />
                     <Route path='/registrar' element={<Registrar />} />
                 </Routes>
-            </Router>
+            </BrowserRouter>
 
         </div>
     );
